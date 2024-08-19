be5aeb90a83405b8e0c138b26551e968b9db10bd
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-08-19 23:53:59 +0200

    Implement extraction of random video recommendation from tracked tab

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,6 @@
 let youtubeTabID: number | undefined = undefined;
-let playedVideos: string[] = []
+let playedVideos: string[] = [];
+let nextVideoUrl: string | undefined = undefined;
 
 chrome.tabs.onCreated.addListener((tab: chrome.tabs.Tab) => {
     if (tab.url !== undefined || tab.pendingUrl !== undefined) {
@@ -14,6 +15,7 @@ chrome.tabs.onCreated.addListener((tab: chrome.tabs.Tab) => {
 chrome.tabs.onRemoved.addListener((tabId: number) => {
     if (tabId === youtubeTabID) {
         youtubeTabID = undefined;
+        nextVideoUrl = undefined;
         console.log(playedVideos);
     }
 })
@@ -30,6 +32,12 @@ chrome.tabs.onUpdated.addListener((tabId: number, changeInfo: chrome.tabs.TabCha
         console.log(`URL: ${url} => ${video_id}`);
     }
 
+    if (changeInfo.audible === true && nextVideoUrl === undefined) {
+        get_random_recommendation(youtubeTabID).then((video_url: string) => {
+            nextVideoUrl = video_url;
+            console.log(`nextVideoUrl: ${nextVideoUrl}`)
+        })
+    }
 })
 
 function extract_video_id(url: string): string | undefined {
@@ -40,4 +48,25 @@ function extract_video_id(url: string): string | undefined {
     }
 
     return undefined;
+}
+
+async function get_random_recommendation(tab_id: number): Promise<string> {
+    const res = await chrome.scripting.executeScript({
+        target: { tabId: tab_id },
+        func: () => {
+            let recommendations = document.getElementsByTagName("ytd-compact-video-renderer");
+            console.log(recommendations);
+            const i = Math.floor(Math.random() * recommendations.length);
+
+            return recommendations[i].getElementsByTagName("a")[0].href;
+        }
+    });
+
+    const rec_video_url = res[0].result;
+    console.log(`rec_video_url: ${rec_video_url}`);
+    if (rec_video_url !== undefined && rec_video_url !== null) {
+        return rec_video_url;
+    } else {
+        return await get_random_recommendation(tab_id);
+    }
 }
