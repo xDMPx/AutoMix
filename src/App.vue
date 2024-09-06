1039877292a325f61de9b3857c617eea0b92fcc3
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-09-06 17:07:04 +0200

    Refactor extension popup to use Vue's reactivity and lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,50 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { getAutoMixState, setAutoMixState } from "./utils.mjs";
+
+const ensureTheatreMode = ref(false);
+const nextVideoId = ref("");
+
+async function clearPlayedVideos() {
+    console.log(`AutoMixPopup; Clearing played videos`);
+    const state = await getAutoMixState();
+    state.playedVideos = [];
+    await setAutoMixState(state);
+}
+
+async function getEnsureTheatreModeValue(): Promise<boolean> {
+    const state = await getAutoMixState();
+    return state.ensureTheatreMode;
+}
+
+chrome.runtime.onMessage.addListener(async (msg: PopupMessage, _sender, _sendResponse) => {
+    console.log(`AutoMixPopup; Vue Message => `);
+    console.log(msg);
+    if (msg.ensureTheatreMode !== undefined) ensureTheatreMode.value = msg.ensureTheatreMode;
+});
+
+
+onMounted(() => {
+    getEnsureTheatreModeValue().then(
+        (checked) => { ensureTheatreMode.value = checked; console.log(`vue ${checked}`); }
+
+    );
+    getAutoMixState().then(
+        (s) => nextVideoId.value = (s.nextVideoId) ? s.nextVideoId : ""
+    );
+});
 </script>
 
 
 <template>
     <div>Next:<br>
-        <a id="next-video-id"></a>
+        <a id="next-video-id" :href="`https://www.youtube.com/watch?v=${nextVideoId}`">{{ nextVideoId}}</a>
     </div>
     <div>
         <label for="ensureTheatreMode">Ensure Theatre Mode:</label>
-        <input type="checkbox" name="ensureTheatreMode" id="ensureTheatreMode" disabled>
+        <input type="checkbox" name="ensureTheatreMode" id="ensureTheatreMode" v-model="ensureTheatreMode" disabled>
     </div>
-    <button type="button" id="clear-played-button">Clear played videos</button>
+    <button type="button" @click="clearPlayedVideos" id="clear-played-button">Clear played videos</button>
 </template>
 
 <script lang="ts">
